4f0b9653a04355a1a16d8c0a956741a98863457c
Chenika <chhabrachenika@gmail.com>
2026-02-04 14:32:08 +0530

    valid parentheses

---
+++ Stack/ValidParentheses.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            } else {
+                if (st.empty()) return false;
+
+                char top = st.top();
+                st.pop();
+
+                if ((s[i] == ')' && top != '(') ||
+                    (s[i] == ']' && top != '[') ||
+                    (s[i] == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
+
+int main() {
+    Solution sol;
+
+    string s;
+    cin >> s;
+
+    if (sol.isValid(s))
+        cout << "Valid" << endl;
+    else
+        cout << "Invalid" << endl;
+
+    return 0;
+}
