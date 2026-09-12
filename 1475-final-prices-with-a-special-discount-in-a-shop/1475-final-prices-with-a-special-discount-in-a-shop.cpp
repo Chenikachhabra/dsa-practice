937152204b6cde9942746a37c7ba097b1bd39318
chenika <165368585+Chenikachhabra@users.noreply.github.com>
2026-09-12 16:13:26 +0530

    Time: 0 ms (100%), Space: 14.1 MB (55.45%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
+        stack<int> st;
         vector<int> ans;
-        for(int i =0 ; i< prices.size() ; i++){
-            bool found = false;
-            for(int j = i+1 ; j< prices.size(); j++){
-                if(prices[j] <= prices[i]){
-                    ans.push_back(prices[i] - prices[j]);
-                    found = true;
-                    break;
-                }
+        for (int i = prices.size()-1; i >= 0 ; i--) {
+
+            while (!st.empty() && st.top() > prices[i]) {
+                st.pop();
+            }
+
+            if (!st.empty()) {
+                int sum = prices[i] - st.top();
+                ans.push_back(sum);
+               
+            } else {
+                ans.push_back(prices[i]);
                 
             }
-            if(!found){
-                    ans.push_back(prices[i]);
-                   
-                }
+             st.push(prices[i]);
         }
+        reverse(ans.begin() , ans.end());
         return ans;
     }
 };
