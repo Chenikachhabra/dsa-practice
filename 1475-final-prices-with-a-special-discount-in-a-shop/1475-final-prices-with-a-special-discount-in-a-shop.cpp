cd617d1846ac7ef3ed6accd4d4c3890abb7117b0
chenika <165368585+Chenikachhabra@users.noreply.github.com>
2026-09-12 15:35:07 +0530

    Time: 0 ms (100%), Space: 14.1 MB (55.45%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        vector<int> ans;
+        for(int i =0 ; i< prices.size() ; i++){
+            bool found = false;
+            for(int j = i+1 ; j< prices.size(); j++){
+                if(prices[j] <= prices[i]){
+                    ans.push_back(prices[i] - prices[j]);
+                    found = true;
+                    break;
+                }
+                
+            }
+            if(!found){
+                    ans.push_back(prices[i]);
+                   
+                }
+        }
+        return ans;
+    }
+};
